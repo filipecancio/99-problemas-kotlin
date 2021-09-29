a8361f9cc084ad417e4f25cbf624364410f8ac8d
cancio <filipe.cancio@outlook.com>
2021-09-29 15:44:34 -0300

    P01 - problema das n rainhas: criando o problema

--- app/src/main/kotlin/problems/kotlin/miscellaneous/MiscellaneousProblem01.kt
+++ app/src/main/kotlin/problems/kotlin/miscellaneous/MiscellaneousProblem01.kt
@@ -1,4 +1,43 @@
 package problems.kotlin.miscellaneous
 
 class MiscellaneousProblem01 {
+
+    companion object{
+        fun nQueenPuzzle(boardSize:Int): String {
+            return Board(boardSize).toBoardString()
+        }
+
+        data class Queen(
+            val row:Int,
+            val column: Int
+        )
+
+        data class Board(
+            val size: Int,
+            var queenList: List<Queen>
+        ){
+
+            constructor(size: Int): this(size, emptyList()){
+                this.addQueen()
+            }
+
+            private fun addQueen(){
+                val nextColumn = (queenList.map { it.column }.maxOrNull() ?: -1) + 1
+                queenList = 0.until(size).map{Queen(it,nextColumn)}
+            }
+
+            fun toBoardString():String{
+                return 0.until(size).joinToString("\n") {row ->
+                    0.until(size).joinToString(" | ") { col ->
+                        if( queenList.contains(Queen(row,col))) "o" else " "
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun main(){
+    val board = MiscellaneousProblem01.nQueenPuzzle(8)
+    println(board)
 }
---
+++ app/src/test/kotlin/problems/kotlin/miscellaneous/MiscellaneousProblem01Test.kt
@@ -0,0 +1,13 @@
+package problems.kotlin.miscellaneous
+
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.IsEqual
+import org.junit.Test
+import problems.kotlin.list.last
+
+class MiscellaneousProblem01Test {
+
+    @Test
+    fun `achar a solução para o problema`(){
+    }
+}
